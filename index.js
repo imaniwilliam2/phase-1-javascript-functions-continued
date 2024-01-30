0d6664b6b15b8e8852f25434b4e281af406e0152
imaniwilliam2 <154285022+imaniwilliam2@users.noreply.github.com>
2024-01-30 12:30:47 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+function saturdayFun(activity){
+   if (activity) {
+    return `This Saturday, I want to ${activity}!`
+   } else {
+    return "This Saturday, I want to roller-skate!"
+   }
+}
+saturdayFun("bathe my dog")
+
+function mondayWork (location) {
+    if (location){
+        return  `This Monday, I will ${location}.`
+    } else {
+        return "This Monday, I will go to the office."
+    }
+}
+
+function wrapAdjective (wrap = "*") {
+    return function innerResult (adj = "special") {
+        return `You are ${wrap}${adj}${wrap}!`
+    }
+
+}
